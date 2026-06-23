7388c74ab21b5772106eec2adf341f9a61c1d0ec
Jade Ducharme <jade_ducharme@brown.edu>
2026-06-23 14:08:59 -0700

    integrated Galaxy object into main graph

--- Frontend/frontend_utils.py
+++ Frontend/frontend_utils.py
@@ -2,41 +2,43 @@
 File for the DASH interaction side
 '''
 
-from astropy.wcs import WCS
 from astropy.coordinates import SkyCoord
 import numpy as np
 import plotly.graph_objects as go
 import yaml
 from astropy import units as u
-from astropy.coordinates import Angle
+
+from Backend.astro_objects import Galaxy, make_wcs
 
 
-def add_object(fig, ra, dec, name, color="#44D9E9", size=8):
-    """
-    Takes in a go.Figure object and an RA/Dec value (in degrees).
-    Adds a dot at the projected Hammer-Aitoff location.
-    Returns the modified figure.
-    """
-    coord = SkyCoord(ra=ra, dec=dec, unit=(u.hourangle, u.deg))
+def add_object(fig, galaxy, xs, ys, grid, cs, color='red', size=8):
 
-    wcs = _make_wcs()
-    px = wcs.all_world2pix([[coord.ra.deg, coord.dec.deg]], 0)[0]
+    wcs = make_wcs()
+    px = wcs.all_world2pix([[galaxy.coord.ra.deg, galaxy.coord.dec.deg]], 0)[0]
 
-    ra_str  = coord.ra.to_string(unit=u.hourangle, sep=('\u02b0', '\u1d50', '\u02e2'), pad=True)
-    dec_str = coord.dec.to_string(unit=u.deg, sep=('\u00b0', '\u2032', '\u2033'), pad=True, alwayssign=True)
+    ra_str  = galaxy.coord.ra.to_string(unit=u.hourangle, sep=('\u02b0', '\u1d50', '\u02e2'), pad=True)
+    dec_str = galaxy.coord.dec.to_string(unit=u.deg, sep=('\u00b0', '\u2032', '\u2033'), pad=True, alwayssign=True)
+
+    fig.add_trace(go.Heatmap(
+        x=xs, y=ys, z=grid,
+        zmin=0, zmax=1,
+        colorscale=cs,
+        showscale=False,
+    ))
 
     fig.add_trace(go.Scatter(
         x=[px[0]],
         y=[px[1]],
-        mode="markers+text" if name else "markers",
+        mode="markers+text" if galaxy.name else "markers",
         marker=dict(color=color, size=size, symbol="circle",
-                    line=dict(color="white", width=0.5)),
-        text=[name],
+            line=dict(color="white", width=0.5),
+            opacity=0),
+        text=[galaxy.name],
         textposition="top center",
         textfont=dict(color="white", size=10),
-        name=name or f"RA={ra} Dec={dec}",
+        name=galaxy.name or f"RA={galaxy.ra} Dec={galaxy.dec}",
         hovertemplate=(
-            f"<b>{name}</b><br>"
+            f"<b>{galaxy.name}</b><br>"
             f"RA: {ra_str}<br>"
             f"Dec: {dec_str}<extra></extra>"
         ),
@@ -53,25 +55,22 @@ def add_catalog_objects(fig, catalog_path):
         catalog = yaml.safe_load(f)
 
     for obj in catalog["objects"]:
-        fig = add_object(fig, ra=obj["ra"], dec=obj["dec"], name=obj["name"])
+
+        coord  = SkyCoord(ra=obj["ra"], dec=obj["dec"], unit=(u.hourangle, u.deg))
+        galaxy = Galaxy(ra=coord.ra.to(u.deg).value, dec=coord.dec.to(u.deg).value, z=obj["redshift"], name=obj["name"])
+
+        xs, ys, grid, cs = galaxy.prepare_figure_data()
+
+        fig = add_object(fig, galaxy, xs, ys, grid, cs)
 
     return fig
-
-
-def _make_wcs():
-    wcs = WCS(naxis=2)
-    wcs.wcs.crpix = [0, 0]
-    wcs.wcs.cdelt = [1.0, 1.0]
-    wcs.wcs.crval = [180, 0]          # center of projection
-    wcs.wcs.ctype = ["RA---AIT", "DEC--AIT"]
-    return wcs
 
 
 def init_graph(catalog_path):
     """
     Build and return the empty sky-chart figure using astropy
     """
-    wcs        = _make_wcs()
+    wcs        = make_wcs()
     n_pts      = 400
     grid_color = "rgba(255,255,255,0.15)"
     label_color= "rgba(255,255,255,0.55)"
